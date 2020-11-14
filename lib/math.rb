87fb2cfe52c843e4ef923776b070086990767382
Raz Al-Jaf <razaljaf@gmail.com>
2020-11-14 15:35:35 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,25 @@
-def addition(num1, num2)
-
+def addition(num1="2", num2="4")
+  sum= 2+4
+return sum
 end
 
-def subtraction(num1, num2)
-
+def subtraction(num1="4",num2="2")
+  subtraction=4-2
+ return subtraction
 end
-
-def division(num1, num2)
-
+def division(num1="4",num2="2")
+  division=4/2
+  return division
 end
-
-def multiplication(num1, num2)
-
+def multiplication(num1="23",num2="22")
+  multiplication=23*22
+  return multiplication
 end
-
-def modulo(num1, num2)
-
+def modulo(num1="8",num2="2")
+  modulo=2%8
+return modulo
 end
-
-def square_root(num)
-
+def square_root(num="81")
+  square_root= Math.sqrt(81)
+return square_root
 end
