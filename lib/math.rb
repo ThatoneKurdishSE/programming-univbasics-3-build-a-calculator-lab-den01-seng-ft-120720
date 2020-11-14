5f59c0b4d79481b72ba821f4df2a9ac3e10ba32f
Raz Al-Jaf <razaljaf@gmail.com>
2020-11-14 15:38:57 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,21 @@
-def addition(num1="2", num2="4")
-  sum= 2+4
+def addition(num1="5", num2="4")
+  sum= 5+4
 return sum
 end
-def subtraction(num1="4",num2="2")
-  subtraction=4-2
+def subtraction(num1="10",num2="5")
+  subtraction=10-5
  return subtraction
 end
-def division(num1="4",num2="2")
-  division=4/2
+def division(num1="50",num2="2")
+  division=50/2
   return division
 end
-def multiplication(num1="23",num2="22")
-  multiplication=23*22
+def multiplication(num1="4",num2="30")
+  multiplication=4*30
   return multiplication
 end
-def modulo(num1="8",num2="2")
-  modulo=2%8
+def modulo(num1="34",num2="5")
+  modulo=34%5
 return modulo
 end
 def square_root(num="81")
